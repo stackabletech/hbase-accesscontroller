3ceea54b2d393c67a63bc725b4a13af4011b2274
Andrew Kenworthy <andrew.kenworthy@stackable.de>
2024-06-03 17:55:57 +0200

    merge conflict

--- src/main/java/tech/stackable/hbase/AllowAccessController.java
+++ src/main/java/tech/stackable/hbase/AllowAccessController.java
@@ -11,34 +11,56 @@ import org.slf4j.LoggerFactory;
 
 @CoreCoprocessor
 @InterfaceAudience.LimitedPrivate(HBaseInterfaceAudience.CONFIG)
-public class AllowAccessController implements MasterCoprocessor, RegionCoprocessor,
-        RegionServerCoprocessor, AccessControlProtos.AccessControlService.Interface, MasterObserver, RegionObserver,
-        RegionServerObserver, EndpointObserver, BulkLoadObserver {
+public class AllowAccessController
+    implements MasterCoprocessor,
+        RegionCoprocessor,
+        RegionServerCoprocessor,
+        AccessControlProtos.AccessControlService.Interface,
+        MasterObserver,
+        RegionObserver,
+        RegionServerObserver,
+        EndpointObserver,
+        BulkLoadObserver {
 
-    private static final Logger LOG = LoggerFactory.getLogger(AllowAccessController.class);
+  private static final Logger LOG = LoggerFactory.getLogger(AllowAccessController.class);
 
-    @Override
-    public void grant(RpcController controller, AccessControlProtos.GrantRequest request, RpcCallback<AccessControlProtos.GrantResponse> done) {
-        LOG.info("xxx Granting permission");
-    }
+  @Override
+  public void grant(
+      RpcController controller,
+      AccessControlProtos.GrantRequest request,
+      RpcCallback<AccessControlProtos.GrantResponse> done) {
+    LOG.info("xxx Granting permission");
+  }
 
-    @Override
-    public void revoke(RpcController controller, AccessControlProtos.RevokeRequest request, RpcCallback<AccessControlProtos.RevokeResponse> done) {
-        LOG.info("xxx Revoking permission");
-    }
+  @Override
+  public void revoke(
+      RpcController controller,
+      AccessControlProtos.RevokeRequest request,
+      RpcCallback<AccessControlProtos.RevokeResponse> done) {
+    LOG.info("xxx Revoking permission");
+  }
 
-    @Override
-    public void getUserPermissions(RpcController controller, AccessControlProtos.GetUserPermissionsRequest request, RpcCallback<AccessControlProtos.GetUserPermissionsResponse> done) {
-        LOG.info("xxx Getting user permissions");
-    }
+  @Override
+  public void getUserPermissions(
+      RpcController controller,
+      AccessControlProtos.GetUserPermissionsRequest request,
+      RpcCallback<AccessControlProtos.GetUserPermissionsResponse> done) {
+    LOG.info("xxx Getting user permissions");
+  }
 
-    @Override
-    public void checkPermissions(RpcController controller, AccessControlProtos.CheckPermissionsRequest request, RpcCallback<AccessControlProtos.CheckPermissionsResponse> done) {
-        LOG.info("xxx Checking permissions");
-    }
+  @Override
+  public void checkPermissions(
+      RpcController controller,
+      AccessControlProtos.CheckPermissionsRequest request,
+      RpcCallback<AccessControlProtos.CheckPermissionsResponse> done) {
+    LOG.info("xxx Checking permissions");
+  }
 
-    @Override
-    public void hasPermission(RpcController controller, AccessControlProtos.HasPermissionRequest request, RpcCallback<AccessControlProtos.HasPermissionResponse> done) {
-        LOG.info("xxx Checking if permission is granted");
-    }
+  @Override
+  public void hasPermission(
+      RpcController controller,
+      AccessControlProtos.HasPermissionRequest request,
+      RpcCallback<AccessControlProtos.HasPermissionResponse> done) {
+    LOG.info("xxx Checking if permission is granted");
+  }
 }
